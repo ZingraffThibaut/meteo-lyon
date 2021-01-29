26f6a1a493e9281bcf55bbb3ae1266fc028ce82c
Thibaut Z <thibaut.zing@gmail.com>
2021-01-29 14:44:14 +0100

    Second commit
    Liaison API
    Création components
    Listing timecode

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
 <template>
   <div id="app">
     <main>
-
-      <!-- <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Rechercher...">
-      </div> -->
-
-    <div>
-      <h1>Lyon</h1>
-    </div>
-    <div class="weather-wrap">
-      <div class="weather-box">
-        <div class="weather-dealy">
-          <div class="date">{{ dateBuilder() }}</div>
-          <div class="temp">2°C</div>
+      <div>
+        <h1>{{ weather.city.name }}</h1>
+        
+      </div>
+      <div class="weather-wrap">
+        <div class="weather-box">
+          <WeatherDealy v-bind:weather="weather"/>
+          <WeatherWeekly v-for="n in 6" :key="n" v-bind:weather="weather" v-bind:n = "n"/>
         </div>
       </div>
-    </div>
 
     </main>
   </div>
 </template>
 
 <script>
+import WeatherDealy from './components/WeatherDealy.vue'
+import WeatherWeekly from './components/WeatherWeekly.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    WeatherDealy,
+    WeatherWeekly,
   },
   data() {
     return{
        token: '956fea801960b3e2d6685c37cb4f31d5eec5bed67a5f66bc3c31cc7f137575c5',
-
+       url_base: 'https://api.meteo-concept.com/api/forecast/',
+       query: '69381',
+       weather: {},
     }
   },
+  mounted() {
+    this.fetchWeather()
+  },
   methods:{
-    dateBuilder () {
-      let d = new Date();
+    fetchWeather(){
+      fetch(`${this.url_base}daily?token=${this.token}&insee=${this.query}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+    },
+    setResults (results){
+      this.weather = results;
+    },
+    dateBuilder (d) {
+      d = new Date(d);
       let months = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Decembre"];
-      let days = ["Dimanche","Lundi","Mardi","Mercredi","Jundi","Vendredi","Samedi"];
+      let days = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
 
       let day = days[d.getDay()];
       let date = d.getDate();
@@ -48,6 +59,10 @@ export default {
 
       return `${day} ${date} ${month} ${year}`;
     },
+    weatherTransalte(){
+      // let codetemp = [];
+      return 'Beau';
+    }
   }
 }
 </script>
@@ -60,19 +75,55 @@ export default {
 }
 body{
   font-family: 'montserrat', sans-serif;
+  background-image: url('https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80');
+  background-size: cover;
+  background-position: bottom;
 }
 
 main{
   padding: 30px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+h1{
+  color: #fff;
+  font-size: 40px;
+  font-weight: 700;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
 }
 
 .weather-wrap{
-  display: flex;
-  align-items: center;
+  margin-top: 30px;
 }
 
 .weather-box{
-  background-color: rgb(28,32,46);
+  width: 100%;
   color: #fff;
+}
+
+.temp{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.temp-min, .temp-max{
+  padding: 10px 25px;
+  font-size: 18px;
+  font-weight: 500;
+}
+.date{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+  font-weight: 700;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+
+i{
+  color: #fff;
+  font-size: 50px;
+  margin-right: 50px;
 }
 </style>
---
+++ src/components/WeatherWeekly.vue
@@ -0,0 +1,73 @@
+<template>
+   <div class="weather-weekly">
+        <div class="date">
+          <div>{{ dateBuilder(weather.forecast[n].datetime) }}</div>
+          <i :class="weatherTransalte(weather.forecast[n].weather)"></i>
+        </div>
+        
+        <div class="temp">
+            temperature minimum : <br> <div class="temp-min">{{ weather.forecast[n].tmin }}C</div>
+            temperature maximum : <br> <div class="temp-max">{{ weather.forecast[n].tmax }}C</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    weather: {},
+    n: Number,
+  },
+  methods:{
+    dateBuilder (d) {
+      d = new Date(d);
+      let months = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Decembre"];
+      let days = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    },
+    weatherTransalte(time){
+      console.log(time);
+      let icone;
+      switch (time){
+        case 1:case 2:case 3:case 4:case 5:
+          icone = 'fas fa-cloud';
+          break;
+        case 6: case 7:
+          icone = 'fas fa-smog';
+          break;
+        case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 20: case 21: case 22: case 30: case 31: case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 70: case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 210: case 211: case 212: case 230: case 231: case 232:
+          icone = 'fas fa-cloud-showers-heavy';
+          break;
+        case 100: case 101: case 102: case 103: case 104: case 105: case 106: case 107: case 108: case 120: case 121: case 122: case 123: case 124: case 125: case 126: case 127: case 128: case 130: case 131: case 132: case 133: case 134: case 135: case 136: case 137: case 138: case 140: case 141: case 142: case 235:
+          icone = 'fas fa-bolt';
+          break;
+        case 60: case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 220: case 221: case 222:
+          icone = 'fas fa-snowflake';
+          break;
+        default:
+          icone = 'fas fa-sun';
+      }
+      return icone;
+    }
+  }
+}
+</script>
+
+<style>
+.weather-weekly{
+  width: 100%;
+  padding: 30px;
+  background-color: rgba(28,32,46,0.75);
+}
+.weather-weekly:last-child{
+  border-radius: 0px 0px 30px 30px;
+}
+</style>
